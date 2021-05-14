994c6d80fa5536ac507c551d6be39b1dc95ff947
wu4f <wuchang@pdx.edu>
2021-05-14 13:40:23 -0700

    Update buildindex.sh
    for capstone team

--- labs/buildindex.sh
+++ labs/buildindex.sh
@@ -1,5 +1,7 @@
-export PATH="/usr/local/home/wuchang/go/src/github.com/googlecodelabs/tools/site/node_modules/.bin:$PATH"
+export PATH="$HOME/go/src/github.com/googlecodelabs/tools/site/node_modules/.bin:$PATH"
+export CODELAB_LOC="$HOME/codelabs_pdx"
 #gulp build -f ../tools/site/gulpfile.js --base-url=https://thefengs.com/wuchang/courses/cs410b/labindex --codelabs-dir=/usr/local/home/wuchang/courses/cs410b/labs --codelabs-namespace="/wuchang/courses/cs410b/labs"
-gulp dist -f ../tools/site/gulpfile.js --codelabs-dir=/usr/local/home/wuchang/claat/labs --codelabs-namespace="/labs"
-rsync -a -I ~/claat/tools/site/dist/ /var/www/html/codelabs
-chmod -R go+rX /var/www/html/codelabs .
+#gulp dist -f ../tools/site/gulpfile.js --codelabs-dir=/usr/local/home/wuchang/claat/labs --codelabs-namespace="/labs"
+gulp dist -f ../tools/site/gulpfile.js --codelabs-dir=$CODELAB_LOC/labs --codelabs-namespace="/labs"
+rsync -a -I $CODELAB_LOC/tools/site/dist/ /var/www/html
+chmod -R go+rX /var/www/html .
